c3f890918f566e1d8698b9dba6cd1a8c701ec58c
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2025-07-03 11:48:04 +0200

    chore(SwiftUI): created DailyScrum model

---
+++ SwiftUI/Scrumdinger/Scrumdinger/Models/DailyScrum.swift
@@ -0,0 +1,16 @@
+//
+//  DailyScrum.swift
+//  Scrumdinger
+//
+//  Created by Erik Sebastian de Erice Jerez on 3/7/25.
+//
+
+import Foundation
+import ThemeKit
+
+struct DailyScrum {
+    var title: String
+    var attendees: [String]
+    var lengthMinutes: Int
+    var theme: Theme
+}
