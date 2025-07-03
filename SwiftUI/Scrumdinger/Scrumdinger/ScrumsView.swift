4331481fde50aaf1f9cbdc711fbc91784b37d4ba
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2025-07-03 12:09:54 +0200

    chore(SwiftUI): displaying a list of daily scrums

---
+++ SwiftUI/Scrumdinger/Scrumdinger/ScrumsView.swift
@@ -0,0 +1,23 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Erik Sebastian de Erice Jerez on 3/7/25.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        List(scrums, id: \.title) { scrum in
+            CardView(scrum: scrum)
+                .listRowBackground(scrum.theme.mainColor)
+        }
+    }
+}
+
+#Preview {
+    ScrumsView(scrums: DailyScrum.sampleData)
+}
