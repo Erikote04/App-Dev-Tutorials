71e012e28cc32759ea58990b064c168d3d45f3b8
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2025-07-10 17:10:24 +0200

    chore(UIKit): updating the snapshot

--- UIKit/Today/Today/ListViewController/ReminderListViewController+DataSource.swift
+++ UIKit/Today/Today/ListViewController/ReminderListViewController+DataSource.swift
@@ -11,6 +11,18 @@ extension ReminderListViewController {
     typealias DataSource = UICollectionViewDiffableDataSource<Int, Reminder.ID>
     typealias Snapshot = NSDiffableDataSourceSnapshot<Int, Reminder.ID>
     
+    func updateSnapshot(reloading ids: [Reminder.ID] = []) {
+        var snapshot = Snapshot()
+        snapshot.appendSections([0])
+        snapshot.appendItems(reminders.map { $0.id })
+        
+        if !ids.isEmpty {
+            snapshot.reloadItems(ids)
+        }
+        
+        dataSource?.apply(snapshot)
+    }
+    
     func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, id: Reminder.ID) {
         let reminder = getReminder(withId: id)
         var contentConfiguration = cell.defaultContentConfiguration()
@@ -46,6 +58,7 @@ extension ReminderListViewController {
         var reminder = getReminder(withId: id)
         reminder.isComplete.toggle()
         updateReminder(reminder)
+        updateSnapshot(reloading: [id])
     }
     
     private func doneButtonConfiguration(for reminder: Reminder) -> UICellAccessory.CustomViewConfiguration {
