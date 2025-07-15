a75ad423fad08fb39e1c47d9ea1caa7d38541f78
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2025-07-15 12:01:56 +0200

    chore(UIKit): added working reminder

--- UIKit/Today/Today/DetailViewController/ReminderViewController.swift
+++ UIKit/Today/Today/DetailViewController/ReminderViewController.swift
@@ -12,10 +12,12 @@ class ReminderViewController: UICollectionViewController {
     private typealias Snapshot = NSDiffableDataSourceSnapshot<Section, Row>
     
     var reminder: Reminder
+    var workingReminder: Reminder
     private var dataSource: DataSource?
     
     init(reminder: Reminder) {
         self.reminder = reminder
+        self.workingReminder = reminder
         
         var listConfiguration = UICollectionLayoutListConfiguration(appearance: .insetGrouped)
         listConfiguration.showsSeparators = false
@@ -48,7 +50,7 @@ class ReminderViewController: UICollectionViewController {
     
     override func setEditing(_ editing: Bool, animated: Bool) {
         super.setEditing(editing, animated: animated)
-        editing ? updateSnapshotForEditing() : updateSnapshotForViewing()
+        editing ? prepareForEditing() : prepareForViewing()
     }
     
     func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, row: Row) {
@@ -66,6 +68,10 @@ class ReminderViewController: UICollectionViewController {
         cell.tintColor = .todayPrimaryTint
     }
     
+    private func prepareForEditing() {
+        updateSnapshotForEditing()
+    }
+    
     private func updateSnapshotForEditing() {
         var snapshot = Snapshot()
         snapshot.appendSections([.title, .date, .notes])
@@ -73,6 +79,14 @@ class ReminderViewController: UICollectionViewController {
         snapshot.appendItems([.header(Section.date.name), .editableDate(reminder.dueDate)], toSection: .date)
         snapshot.appendItems([.header(Section.notes.name), .editableText(reminder.notes)], toSection: .notes)
         dataSource?.apply(snapshot)
+    }
+    
+    private func prepareForViewing() {
+        if workingReminder != reminder {
+            reminder = workingReminder
+        }
+        
+        updateSnapshotForViewing()
     }
     
     private func updateSnapshotForViewing() {
