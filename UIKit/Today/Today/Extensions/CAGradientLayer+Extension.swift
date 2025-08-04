53048d8ff4b80ff924fa067bb7979e49c9d7ef3e
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2025-08-04 14:24:53 +0200

    chore(UIKit): created gradient layer

---
+++ UIKit/Today/Today/Extensions/CAGradientLayer+Extension.swift
@@ -0,0 +1,36 @@
+//
+//  CAGradientLayer+Extension.swift
+//  Today
+//
+//  Created by Erik Sebastian de Erice Jerez on 4/8/25.
+//
+
+import UIKit
+
+extension CAGradientLayer {
+    static func gradientLayer(for style: ReminderListStyle, in frame: CGRect) -> Self {
+        let layer = Self()
+        layer.colors = colors(for: style)
+        layer.frame = frame
+        return layer
+    }
+    
+    private static func colors(for style: ReminderListStyle) -> [CGColor] {
+        let beginColor: UIColor
+        let endColor: UIColor
+        
+        switch style {
+        case .all:
+            beginColor = .todayGradientAllBegin
+            endColor = .todayGradientAllEnd
+        case .future:
+            beginColor = .todayGradientFutureBegin
+            endColor = .todayGradientFutureEnd
+        case .today:
+            beginColor = .todayGradientTodayBegin
+            endColor = .todayGradientTodayEnd
+        }
+        
+        return [beginColor.cgColor, endColor.cgColor]
+    }
+}
