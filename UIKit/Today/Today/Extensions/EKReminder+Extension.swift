045789180c86d29c551776fea98606fff3c852fe
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2025-08-05 14:07:30 +0200

    chore(UIKit): converting between model types

---
+++ UIKit/Today/Today/Extensions/EKReminder+Extension.swift
@@ -0,0 +1,29 @@
+//
+//  EKReminder+Extension.swift
+//  Today
+//
+//  Created by Erik Sebastian de Erice Jerez on 5/8/25.
+//
+
+import EventKit
+import Foundation
+
+extension EKReminder {
+    func update(using reminder: Reminder, in store: EKEventStore) {
+        title = reminder.title
+        notes = reminder.notes
+        isCompleted = reminder.isComplete
+        calendar = store.defaultCalendarForNewReminders()
+        alarms?.forEach { alarm in
+            guard let absoluteDate = alarm.absoluteDate else { return }
+            let comparison = Locale.current.calendar.compare(reminder.dueDate, to: absoluteDate, toGranularity: .minute)
+            if comparison != .orderedSame {
+                removeAlarm(alarm)
+            }
+        }
+        
+        if !hasAlarms {
+            addAlarm(EKAlarm(absoluteDate: reminder.dueDate))
+        }
+    }
+}
