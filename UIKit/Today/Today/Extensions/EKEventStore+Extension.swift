091f7c0f3a7d4a1c31cd1479a345e739ab61ccd1
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2025-08-05 12:27:16 +0200

    chore(UIKit): reading all reminders

--- UIKit/Today/Today/Extensions/EKEventStore+Extension.swift
+++ UIKit/Today/Today/Extensions/EKEventStore+Extension.swift
@@ -9,7 +9,7 @@ import EventKit
 import Foundation
 
 extension EKEventStore {
-    func reminders(matching predicate: NSPredicate) async throws -> [EKReminder] {
+    func getReminders(matching predicate: NSPredicate) async throws -> [EKReminder] {
         try await withCheckedThrowingContinuation { continuation in
             fetchReminders(matching: predicate) { reminders in
                 if let reminders {
