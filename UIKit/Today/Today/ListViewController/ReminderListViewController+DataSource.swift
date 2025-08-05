c0cef40b6e56548d8da2c9a182a997a484f1b773
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2025-08-05 16:07:08 +0200

    chore(UIKit): removing reminders

--- UIKit/Today/Today/ListViewController/ReminderListViewController+DataSource.swift
+++ UIKit/Today/Today/ListViewController/ReminderListViewController+DataSource.swift
@@ -89,8 +89,12 @@ extension ReminderListViewController {
     }
     
     func deleteReminder(withId id: Reminder.ID) {
-        let index = reminders.indexOfReminder(withId: id)
-        reminders.remove(at: index)
+        do {
+            try reminderStore.remove(with: id)
+            let index = reminders.indexOfReminder(withId: id)
+            reminders.remove(at: index)
+        } catch TodayError.accessDenied {
+        } catch { showError(error) }
     }
     
     func prepareReminderStore() {
