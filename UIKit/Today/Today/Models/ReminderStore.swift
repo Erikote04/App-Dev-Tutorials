2d7318d9ba24c979780bb13478aca0faca66aeca
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2025-08-05 13:55:46 +0200

    chore(UIKit): reading reminders individually

--- UIKit/Today/Today/Models/ReminderStore.swift
+++ UIKit/Today/Today/Models/ReminderStore.swift
@@ -52,4 +52,12 @@ final class ReminderStore {
         
         return reminders
     }
+    
+    private func read(with id: Reminder.ID) throws -> EKReminder {
+        guard let ekReminder = ekStore.calendarItem(withIdentifier: id) as? EKReminder else {
+            throw TodayError.failedReadingCalendarItem
+        }
+        
+        return ekReminder
+    }
 }
